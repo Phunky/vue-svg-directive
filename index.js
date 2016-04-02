4b23f410b0bc11a50c73c729e911ca44a725e302
Peter Matkovsky <peter@matkovsky.com>
2016-04-02 22:42:39 +0200

    Linting added, tabsize changed to 4

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
 export default {
-  install (Vue, opts) {
-    Vue.directive('svg', {
-      params: ['sprite'],
+    install(Vue, opts) {
+        Vue.directive('svg', {
+            params: ['sprite'],
 
-      bind () {
-        if(!opts.prefix) {
-          opts.prefix = '';
-        }
+            bind() {
+                if (!opts.prefix) {
+                    opts.prefix = '';
+                }
 
-        let svg    = document.createElementNS('http://www.w3.org/2000/svg', 'svg'),
-            use    = document.createElementNS('http://www.w3.org/2000/svg', 'use'),
-            sprite = `${opts.prefix}${this.params.sprite}`,
-            scope  = null,
-            el     = this.el;
+                let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+                let use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+                let sprite = `${opts.prefix}${this.params.sprite}`;
+                let scope = null;
+                let el = this.el;
 
-        for (let key in el.attributes) {
-          let attr = el.attributes[key].name;
-          if( typeof attr === 'string' && attr.substr(0,2) === '_v' ) scope = attr;
-        }
+                for (let key in el.attributes) {
+                    let attr = el.attributes[key].name;
 
-        use.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', `${opts.sprites}#${sprite}`);
-        use.setAttributeNS(null, scope, '');
+                    if (typeof attr === 'string' && attr.substr(0, 2) === '_v') {
+                        scope = attr;
+                    }
+                }
 
-        if (el.tagName !== 'svg') {
-          svg.setAttributeNS(null, scope, '');
-          if (opts.class) svg.classList.add(opts.class, sprite);
-          el.appendChild(svg);
-          el = svg;
-        }
+                use.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', `${opts.sprites}#${sprite}`);
+                use.setAttributeNS(null, scope, '');
 
-        if (opts.class) el.classList.add(opts.class, sprite);
-        el.appendChild(use);
-      }
-    })
-  }
-}
+                if (el.tagName !== 'svg') {
+                    svg.setAttributeNS(null, scope, '');
+                    if (opts.class) svg.classList.add(opts.class, sprite);
+                    el.appendChild(svg);
+                    el = svg;
+                }
+
+                if (opts.class) el.classList.add(opts.class, sprite);
+                el.appendChild(use);
+            }
+        });
+    }
+};
